c352c9915ab9bf71968f9c4888f47767b4d384a5
NagachandanP <87109539+NagachandanP@users.noreply.github.com>
2025-04-15 11:33:23 +0530

    Create write-lines.py

---
+++ write-lines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import time
+
+file_path = '/home/omnia/v4-test-progress/monitored_file.txt'
+
+for i in range(1, 6):
+    msg = f"Line {i}: hiiii"
+    print(msg, flush=True)
+    with open(file_path, 'a') as file:
+        file.write(msg + '\n')
+    time.sleep(2)
